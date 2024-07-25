d4970a10d37cca16404d06205444a98db2883a6e
mohdanas1234 <anas@ag4444>
2024-07-26 01:30:50 +0530

    update

---
+++ src/components/Payment.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Payment = () => {
+  return (
+     <>
+    <div className="w-full bg-slate-100">
+    <div className="w-full md:w-[60%]  mx-auto px-4 md:px-0 py-12">
+    
+    <div id="paypal-container-3PNS4KXM2FLDA"></div>
+    
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Payment
